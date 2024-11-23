e44cf0a5500dc5c68225a85a22652d04b8fd871f
jdoe <e3kskoy7wqk@163.com>
2024-11-23 22:59:50 +0800

    fix: 修复了DeclPrinter中的一个错误，该错误导致PrintRawDeclStmt调用了错误的printGroup函数

--- clang/tools/translator/parser/lib/ASTParse.cpp
+++ clang/tools/translator/parser/lib/ASTParse.cpp
@@ -1789,7 +1789,7 @@ void DeclPrinter::PrintRawDecl(Decl *D) {
 
 void DeclPrinter::PrintRawDeclStmt(const DeclStmt *S) {
   SmallVector<Decl *, 2> Decls(S->decls());
-  Decl::printGroup(Decls.data(), Decls.size(), Out, Policy, Indentation);
+  printGroup(Decls.data(), Decls.size(), Out, Policy, Indentation);
 }
 
 void DeclPrinter::VisitNullStmt(NullStmt *Node) {
