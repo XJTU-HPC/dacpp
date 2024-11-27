bedf9d6c2c90ad37e2ffbd23625691afc31e3162
jdoe <e3kskoy7wqk@163.com>
2024-11-27 23:47:41 +0800

    Revert "添加了一个函数，可以使将Split映射到VNode"

--- clang/tools/translator/parser/include/Shell.h
+++ clang/tools/translator/parser/include/Shell.h
@@ -65,7 +65,6 @@ public:
 
     void parseShell(const BinaryOperator* dacExpr, std::vector<std::vector<int>> shapes);
     void GetBindInfo(std::vector<BINDINFO> *pbindInfo) ;
-    VNode * search_symbol(const clang::ValueDecl *) ;
 
     ALGraph *G;
 };
