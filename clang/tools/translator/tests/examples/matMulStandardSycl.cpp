d9d1115a730c60219ce787fa0f23fca77fc1eeed
XDUqinian <1841128521@qq.com>
2025-01-03 14:40:37 +0800

    feat:dataRecfon use new tensor and modify the old tests

--- clang/tools/translator/tests/examples/matMulStandardSycl.cpp
+++ clang/tools/translator/tests/examples/matMulStandardSycl.cpp
@@ -13,7 +13,7 @@ typedef std::vector<std::any> list;
 
 
 #include <sycl/sycl.hpp>
-#include "DataReconstructor.h"
+#include "/data/qinian/ice/dacpp/clang/tools/translator/dpcppLib/include/DataReconstructor.old.h"
 
 using namespace sycl;
 
--- clang/tools/translator/tests/src/DataReconstructorTest5.cpp
+++ clang/tools/translator/tests/src/DataReconstructorTest5.cpp
@@ -6,9 +6,13 @@ void debug(int num){
 }
 int main(){
     std::vector<int> data{90,100,110,120,202,228,254,280,314,356,398,440,426,484,542,600};
-    std::vector<int> shape{4,4};
-    dacpp::Tensor<int> matC(data,shape);
+    // std::vector<int> shape{4,4};
+    dacpp::Tensor<int,2> matC({4,4},data);
     matC.print();
+
+    DataInfo info_matC;
+    info_matC.dim = matC.getDim();
+    for(int i = 0; i < info_matC.dim; i++) info_matC.dimLength.push_back(matC.getShape(i));
 
     RegularSlice si = RegularSlice("si", 2, 2);
 	si.SetSplitSize(2);
@@ -24,8 +28,8 @@ int main(){
     sj.setDimId(1);
     sj.setSplitLength(4);
     matC_ops.push_back(sj);
-    matC_tool.init(matC,matC_ops);
-    matC_tool.Reconstruct(r_matC);                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
+    matC_tool.init(info_matC,matC_ops);
+    matC_tool.Reconstruct(r_matC,matC);                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
   
     for(int i=0;i<16;i++) std::cout<<r_matC[i]<<" ";
     std::cout<<"\n";
@@ -35,8 +39,8 @@ int main(){
     for(int i=0;i<16;i++) std::cout<<r_matC[i]<<" ";
     std::cout<<"\n";
 
-    dacpp::Tensor<int> res=matC_tool.UpdateData(r_matC);
-    res.print();
+    matC_tool.UpdateData(r_matC,matC);
+    matC.print();
 
     return 0;
 }
