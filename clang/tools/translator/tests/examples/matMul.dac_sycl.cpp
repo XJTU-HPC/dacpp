c58d77f83dc4e88be4702a4f5874d92c64438234
XDUqinian <1841128521@qq.com>
2025-01-06 18:50:31 +0800

    fix: delete absolute path

--- clang/tools/translator/tests/examples/matMul.dac_sycl.cpp
+++ clang/tools/translator/tests/examples/matMul.dac_sycl.cpp
@@ -1,19 +1,7 @@
 #include <vector>
-
-#include "/data/zjx/dacpp/clang/tools/translator/dacppLib/include/Tensor.hpp"
-
-using dacpp::Tensor;
-
-namespace dacpp {
-typedef std::vector<std::any> list;
-}
-
-
-
-
-
 #include <sycl/sycl.hpp>
 #include "/data/qinian/ice/dacpp/clang/tools/translator/dpcppLib/include/DataReconstructor.old.h"
+using dacpp::Tensor;
 
 using namespace sycl;
 
